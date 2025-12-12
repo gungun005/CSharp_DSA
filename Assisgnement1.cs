6ac7c186aacc251502fa14fd01336e67599ce766
Gungun <120474972+gungun005@users.noreply.github.com>
2025-12-12 15:46:18 +0530

    Create Assisgnement1.cs
    Project: Student Profile System
    Create a Student class with:
    Fields: firstName, lastName, age, grade
    Constructor that initializes all properties
    Method GetFullInfo() returning a formatted string
    Create 3 student objects and print their info
    Concepts mastered:
    ➡️ Class creation, object instantiation, constructors, fields, methods

---
+++ Assisgnement1.cs
@@ -0,0 +1,31 @@
+//Assignment 1 — Classes, Objects & Constructors
+using System;
+class Student
+{
+private string fname;
+private string lname;
+int age;
+char gender;
+public Student(string Fname,string Lname,int Age,char Gender)
+{
+this.fname=Fname;
+this.lname=Lname;
+this.age=Age;
+this.gender=Gender;
+}
+public void GetFullInfo()
+{
+Console.WriteLine($"{fname}|{lname}|{age}|{gender}");
+}
+}
+class Program{
+public static void Main()
+{
+Student s1=new Student("Gungun","S",22,'a');
+Student s2=new Student("Hancy","L",22,'a');
+Student s3=new Student("Devanshi","M",22,'a');
+s1.GetFullInfo();
+s2.GetFullInfo();
+s3.GetFullInfo();
+}
+}
